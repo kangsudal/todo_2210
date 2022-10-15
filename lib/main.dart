ebbc29f74d62357ede5d44dcb213b0e8903742ce
kangsudal_notebook <kangsudal@gmail.com>
2022-10-15 16:37:52 +0900

    flutter앱에 firebase 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_sqlite/screens/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
